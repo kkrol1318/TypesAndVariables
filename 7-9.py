09b852fe30e0fd21bff716fbfca9038abfb9971d
kkrol1318 <kkrol.1325@wp.pl>
2024-11-16 21:10:56 +0100

    Update 7-9.py

--- 7-9.py
+++ 7-9.py
@@ -1,2 +1,7 @@
-Dice rolled: 4
-Special number (1 or 6): False
+import random
+
+dice_roll = random.randint(1, 6)
+special = dice_roll in (1, 6)
+
+print(f"""Dice roll: {dice_roll}
+Special number (1 or 6): {special}""")
